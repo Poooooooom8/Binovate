7d3e8f5927f38c60628c2a126a6791b134df6e8c
SundayIT23 <68070168@kmitl.ac.th>
2025-10-07 21:53:06 +0700

    commit models.py

---
+++ Backend/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.dialects.postgresql import UUID
+from .database import Base
+import uuid
+
+class User(Base): #Inherit จาก Base ของ Database เพื่อแมปให้ตรงกับ Table ของ Database
+    """For create or connect to User table"""
+    __tablename__ = "users"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(String, nullable=True)
+
+class Bin(Base):
+    """For create or connect to Bin table"""
+    __tablename__ = "bins"
+    bin_id = Column(String, unique=True, nullable=True, primary_key=True)
+    status = Column(String, nullable=False)
+
+class UserBin(Base):
+    """For create or connect to UserBin table"""
+    __tablename__ = "user_bins"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), default=uuid.uuid4)
+    bin_id = Column(String, nullable=True)
